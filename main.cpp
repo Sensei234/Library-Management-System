f3033b23555dccc2c9b240b369791bbe53ad700e
Sensei234 <74450976+Sensei234@users.noreply.github.com>
2024-12-04 17:32:08 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,18 @@ struct Book
     string title;
     string author;
     int year;
+    int pages;
+    float price;
 
     void display() const
     {
-        cout << "ID: " << id << ", Title: " << title << ", Author: " << author << ", Year: " << year << endl;
+        cout << "ID: " << id
+             << ", Title: " << title
+             << ", Author: " << author
+             << ", Year: " << year
+             << ", Pages: " << pages
+             << ", Price: $" << price
+             << endl;
     }
 };
 
@@ -28,7 +36,12 @@ private:
         ofstream file(FILENAME);
         for (const auto &book : books)
         {
-            file << book.id << "|" << book.title << "|" << book.author << "|" << book.year << "\n";
+            file << book.id << "|"
+                 << book.title << "|"
+                 << book.author << "|"
+                 << book.year << "|"
+                 << book.pages << "|"
+                 << book.price << "\n";
         }
         file.close();
     }
@@ -45,11 +58,15 @@ private:
                 size_t pos1 = line.find("|");
                 size_t pos2 = line.find("|", pos1 + 1);
                 size_t pos3 = line.find("|", pos2 + 1);
+                size_t pos4 = line.find("|", pos3 + 1);
+                size_t pos5 = line.find("|", pos4 + 1);
 
                 book.id = stoi(line.substr(0, pos1));
                 book.title = line.substr(pos1 + 1, pos2 - pos1 - 1);
                 book.author = line.substr(pos2 + 1, pos3 - pos2 - 1);
-                book.year = stoi(line.substr(pos3 + 1));
+                book.year = stoi(line.substr(pos3 + 1, pos4 - pos3 - 1));
+                book.pages = stoi(line.substr(pos4 + 1, pos5 - pos4 - 1));
+                book.price = stof(line.substr(pos5 + 1));
 
                 books.push_back(book);
             }
@@ -72,6 +89,10 @@ public:
         getline(cin, book.author);
         cout << "Enter Year: ";
         cin >> book.year;
+        cout << "Enter Pages: ";
+        cin >> book.pages;
+        cout << "Enter Price: ";
+        cin >> book.price;
         books.push_back(book);
         saveToFile();
         cout << "Book added successfully!" << endl;
@@ -121,6 +142,27 @@ public:
         }
         cout << "Book not found!" << endl;
     }
+
+    void listBookByAuthor()
+    {
+        string authorName;
+        cout << "Enter Author Name: ";
+        cin.ignore();
+        getline(cin, authorName);
+
+        for (const auto &book : books)
+        {
+            if (book.author == authorName)
+            {
+                book.display();
+            }
+        }
+    }
+
+    void countBooks()
+    {
+        cout << "\nNumber of books in the library: " << books.size() << endl;
+    }
 };
 
 int main()
@@ -131,7 +173,13 @@ int main()
     do
     {
         cout << "\n Library Management System \n";
-        cout << "1. Add Book\n2. Search Book\n3. Display Books\n4. Delete Book\n5. Exit\n";
+        cout << "1. Add Book\n";
+        cout << "2. Search Book\n";
+        cout << "3. Display Books\n";
+        cout << "4. Delete Book\n";
+        cout << "5. List Books by Author\n";
+        cout << "6. Count Books\n";
+        cout << "7. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -150,12 +198,18 @@ int main()
             library.deleteBook();
             break;
         case 5:
+            library.listBookByAuthor();
+            break;
+        case 6:
+            library.countBooks();
+            break;
+        case 7:
             cout << "Existing...\n";
             break;
         default:
             cout << "Invalid choice. Try again.\n";
         }
-    } while (choice != 5);
+    } while (choice != 7);
 
     return 0;
 }
